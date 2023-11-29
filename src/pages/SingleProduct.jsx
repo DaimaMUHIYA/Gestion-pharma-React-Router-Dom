30200edf9aeb2e00e5e689ff9b0ea29b3789b8f1
DaimaMUHIYA <muhiyabenjamin@gmail.com>
2023-11-29 19:35:10 +0100

    creation of the component (which is a page) SingleProduct

---
+++ src/pages/SingleProduct.jsx
@@ -0,0 +1,17 @@
+import { useParams } from "react-router-dom";
+import ProductService from "../models/services/productService";
+import ProductPreview from "../components/productPreview";
+
+function SingleProduct() {
+    const {id} = useParams();
+    const data = ProductService.getProducts();
+    const product = data.find((e) => e.id === id);
+
+    return (
+        <>
+            <ProductPreview product={product} seeMore={true}></ProductPreview>
+        </>
+    );
+}
+
+export default SingleProduct;
